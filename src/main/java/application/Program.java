cc6cdf379df36c8c7d9ff521e2be13df5caeccbc
LucasMS21 <lucasedu.6a@gmail.com>
2026-07-13 15:07:08 -0300

    Seller entity class creation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
 
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(2002, 03,21), 3000.00, obj);
+        System.out.println(seller);
     }
 }
